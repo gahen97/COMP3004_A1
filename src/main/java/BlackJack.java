7d22bbe0646f3f6fefa3e01a1da8239303afb572
gahen97 <gahen97@gmail.com>
2018-09-19 15:55:43 -0400

    Added methods to BlackJack class

--- src/main/java/BlackJack.java
+++ src/main/java/BlackJack.java
@@ -1,5 +1,7 @@
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class BlackJack {
@@ -20,12 +22,35 @@ public class BlackJack {
 		deck.shuffle();
 	}
 	
-	public void fileMode() {
-		file = new ArrayList<String>();
+	public void fileMode(File input) {
+	
+		Scanner in;
+		String line = "";
+		try {
+			in = new Scanner(input);
+			line = in.nextLine();
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		file = new ArrayList<String> (Arrays.asList(line.split("\\s+")));
 	}
 	
 	public Deck getDeck() {
 		return deck;
+	}
+	
+	public ArrayList<String> getFile(){
+		return file;
+	}
+	
+	public Hand getPlayerHand(){
+		return player;
+	}
+	
+	public Hand getDealerHand(){
+		return player;
 	}
 	
 	public static void main(String[] args) {
@@ -33,9 +58,33 @@ public class BlackJack {
 		Scanner user = new Scanner(System.in);
 		
 		System.out.println("Would you like to continue in console (c) mode or file (f) mode?");
-		if (user.nextLine() == "f")
-			game.fileMode();
-		else
-			game.consoleMode();
+		while (true) {
+			if (user.nextLine() == "f") {
+				game.fileMode(game.promptForFile(user));
+				break;
+			}
+			else if (user.nextLine() == "c") {
+				game.consoleMode();
+				break;
+			}
+			else {
+				System.out.println("Invalid input, please type in c or f");
+			}
+		}
+		
+		user.close();
+	}
+	
+	private File promptForFile(Scanner user) {
+		System.out.println("Please enter in the path to the file");
+		File file;
+		while (true) {
+			file = new File(user.nextLine());
+			if (file.exists())
+				return file;
+			else {
+				System.out.println("Invalid file, please enter in your file path again");
+			}
+		}
 	}
 }
