7c56100f4e97f72ea1373647fa2477b5ea49ca45
gahen97 <gahen97@gmail.com>
2018-09-19 04:19:59 -0400

    Added a few more test cases to CardTest

--- src/test/java/CardTest.java
+++ src/test/java/CardTest.java
@@ -20,43 +20,45 @@ public class CardTest extends TestCase{
 		Card card1 = new Card("S2");
 		assertEquals("Two", card1.getRank());
 		
-		Card card2 = new Card("S3");
+		Card card2 = new Card("H3");
 		assertEquals("Three", card2.getRank());
 		
-		Card card3 = new Card("S4");
+		Card card3 = new Card("C4");
 		assertEquals("Four", card3.getRank());
 		
-		Card card4 = new Card("S5");
+		Card card4 = new Card("D5");
 		assertEquals("Five", card4.getRank());
 		
 		Card card5 = new Card("S6");
 		assertEquals("Six", card5.getRank());
 		
-		Card card6 = new Card("S7");
+		Card card6 = new Card("H7");
 		assertEquals("Seven", card6.getRank());
 		
-		Card card7 = new Card("S8");
+		Card card7 = new Card("C8");
 		assertEquals("Eight", card7.getRank());
 		
-		Card card8 = new Card("S9");
+		Card card8 = new Card("D9");
 		assertEquals("Nine", card8.getRank());
 		
-		Card card9 = new Card("SJ");
-		assertEquals("Jack", card9.getRank());
+		Card card9 = new Card("S10");
+		assertEquals("Nine", card9.getRank());
 		
-		Card card10 = new Card("SQ");
-		assertEquals("Queen", card10.getRank());
+		Card card10 = new Card("HJ");
+		assertEquals("Jack", card10.getRank());
 		
-		Card card11 = new Card("SK");
-		assertEquals("King", card11.getRank());
+		Card card11 = new Card("CQ");
+		assertEquals("Queen", card11.getRank());
 		
-		Card card12 = new Card("SA");
-		assertEquals("Ace", card12.getRank());
+		Card card12 = new Card("DK");
+		assertEquals("King", card12.getRank());
+		
+		Card card13 = new Card("SA");
+		assertEquals("Ace", card13.getRank());
 	}
 	
 	public void testToString() {
 		Card card0 = new Card("SK");
-		System.out.println(card0.toString());
 		assertEquals("King of Spades", card0.toString());
 		
 		Card card1 = new Card("HA");
