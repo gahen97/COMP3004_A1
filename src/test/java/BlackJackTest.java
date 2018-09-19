56e9fa705abff57401ebab2ed9873b0652efedbe
gahen97 <gahen97@gmail.com>
2018-09-19 15:45:16 -0400

    Testing file is read properly

--- src/test/java/BlackJackTest.java
+++ src/test/java/BlackJackTest.java
@@ -1,3 +1,5 @@
+import java.io.File;
+
 import junit.framework.TestCase;
 
 public class BlackJackTest extends TestCase{
@@ -5,14 +7,24 @@ public class BlackJackTest extends TestCase{
 	public void testConsoleMode() {
 		BlackJack game = new BlackJack();
 		game.consoleMode();
+		
+		assertNull(game.getFile());
 		assertNotNull(game.getDeck());
 		
 	}
 	
 	public void testFileMode() {
 		BlackJack game = new BlackJack();
-		game.fileMode();
+		File test = new File("C:\\Users\\Gahen\\eclipse-workspace\\COMP3004_A1\\src\\test\\resources\\inputTest0.txt");
+		game.fileMode(test);
+
 		assertNull(game.getDeck());
+		assertNotNull(game.getFile());
+		assertEquals("SK", game.getFile().get(0));
+		assertEquals("HA", game.getFile().get(1));
+		assertEquals("HQ", game.getFile().get(2));
+		assertEquals("CA", game.getFile().get(3));
 	}
+	
 	
 }
